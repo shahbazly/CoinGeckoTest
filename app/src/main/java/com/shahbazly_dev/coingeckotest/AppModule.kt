a3fa42b2eb5b7ed9bd91e75c9c348b505540738f
shahbazly <shahbazly.dev@gmail.com>
2022-09-25 13:40:03 +0300

    Add logging interceptor

--- app/src/main/java/com/shahbazly_dev/coingeckotest/AppModule.kt
+++ app/src/main/java/com/shahbazly_dev/coingeckotest/AppModule.kt
@@ -1,14 +1,19 @@
 package com.shahbazly_dev.coingeckotest
 
+import com.shahbazly_dev.coingeckotest.ui.data.CoinGeckoService
+import com.shahbazly_dev.coingeckotest.ui.utils.Constants.BASE_URL
+import com.shahbazly_dev.coingeckotest.ui.utils.Constants.CONNECTION_TIMEOUT_SECONDS
+import com.shahbazly_dev.coingeckotest.ui.utils.Constants.READ_TIMEOUT_SECONDS
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
-
-private const val BASE_URL = "https://api.coingecko.com/"
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -16,8 +21,32 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit = Retrofit.Builder()
+    fun provideOkHttpClient(): OkHttpClient {
+        val builder = OkHttpClient.Builder()
+
+        if (BuildConfig.DEBUG)
+            builder
+                .addInterceptor(
+                    HttpLoggingInterceptor().apply {
+                        level = HttpLoggingInterceptor.Level.BODY
+                    }
+                )
+                .connectTimeout(CONNECTION_TIMEOUT_SECONDS, TimeUnit.SECONDS)
+                .readTimeout(READ_TIMEOUT_SECONDS, TimeUnit.SECONDS)
+
+        return builder.build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
+        .client(okHttpClient)
         .build()
+
+    @Provides
+    @Singleton
+    fun provideCoinGeckoService(retrofit: Retrofit): CoinGeckoService =
+        retrofit.create(CoinGeckoService::class.java)
 }
---
+++ app/src/main/java/com/shahbazly_dev/coingeckotest/ui/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.shahbazly_dev.coingeckotest.ui.utils
+
+object Constants {
+    const val BASE_URL = "https://api.coingecko.com/"
+    const val CONNECTION_TIMEOUT_SECONDS = 5L
+    const val READ_TIMEOUT_SECONDS = 5L
+
+
+}
