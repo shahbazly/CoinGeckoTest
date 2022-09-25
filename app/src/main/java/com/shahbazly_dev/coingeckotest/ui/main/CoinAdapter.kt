6d4506d12a903e048bf6d58117956c0c10328450
shahbazly <shahbazly.dev@gmail.com>
2022-09-25 16:38:23 +0300

    Update CoinAdapter.kt

--- app/src/main/java/com/shahbazly_dev/coingeckotest/ui/main/CoinAdapter.kt
+++ app/src/main/java/com/shahbazly_dev/coingeckotest/ui/main/CoinAdapter.kt
@@ -3,16 +3,17 @@ package com.shahbazly_dev.coingeckotest.ui.main
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.ListAdapter
+import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import coil.transform.CircleCropTransformation
 import com.shahbazly_dev.coingeckotest.R
 import by.kirich1409.viewbindingdelegate.viewBinding
+import coil.clear
 import com.shahbazly_dev.coingeckotest.databinding.CoinItemBinding
 import com.shahbazly_dev.coingeckotest.ui.domain.Coin
 
-class CoinAdapter : ListAdapter<Coin, CoinAdapter.ViewHolder>(DiffUtil()) {
+class CoinAdapter : PagingDataAdapter<Coin, CoinAdapter.ViewHolder>(DiffUtil()) {
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         private val viewBinding: CoinItemBinding by viewBinding()
@@ -26,6 +27,14 @@ class CoinAdapter : ListAdapter<Coin, CoinAdapter.ViewHolder>(DiffUtil()) {
             symbolTextView.text = coin.symbol
             priceTextView.text = "${coin.price}"
             changePercentageTextView.text = "${coin.changePercentage}%"
+        }
+
+        fun clear() = with(viewBinding) {
+            avatarImageView.clear()
+            coinTextView.text = ""
+            symbolTextView.text = ""
+            priceTextView.text = ""
+            changePercentageTextView.text = ""
         }
     }
 
@@ -47,6 +56,11 @@ class CoinAdapter : ListAdapter<Coin, CoinAdapter.ViewHolder>(DiffUtil()) {
         return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) =
-        holder.onBind(getItem(position))
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val coin = getItem(position)
+        if (coin != null)
+            holder.onBind(coin)
+        else
+            holder.clear()
+    }
 }
